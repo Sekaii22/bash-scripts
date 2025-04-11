015fd57f21356c0214b9341fe6683474ce101dfb
Sekaii22 <shawn.ng22@gmail.com>
2025-04-11 12:20:46 +0800

    Format scan reports into table format and now always print host's details

--- network-scanner/network_scanner.sh
+++ network-scanner/network_scanner.sh
@@ -1,61 +1,98 @@
 #!/usr/bin/env bash
 
-subnet=$1;
+# scans for any online host (host discovery), then scans the top 1000 ports to check if open (port scan).
+# HOW TO USE: .network_scanner.sh <subnet?=hostSubnet>
 
-# if subnet argument not given, use host's subnet
-if [ -z ${subnet:+x} ]; then
-    echo "Subnet arg not given";
-
-    hostname=$(hostname -I);
-    if [ -z ${hostname:+x} ]; then
-        echo "Error: cannot find host name";
-        echo "exiting...";
-        exit 1;
-    fi
-
-    netmask=$(ifconfig | grep $hostname | awk '{print $4}');
-    echo "hostname: $hostname";
-    echo "netmask: $netmask";
-
-    # parse address into parts 
-    IFS=. read -r h1 h2 h3 h4 <<< $hostname;
-    IFS=. read -r m1 m2 m3 m4 <<< $netmask;
-
-    # bitwise AND operation on hostname and netmask to get network address
-    network=$(echo "$((h1 & m1)).$((h2 & m2)).$((h3 & m3)).$((h4 & m4))");
-    netprefix=$(ip addr show | grep $hostname | awk '{print $2}' | cut -d / -f 2);
-    subnet="${network}/${netprefix}";     # CIDR notation
-    echo "subnet: ${subnet}";
-    echo;
+hostname=$(hostname -I);
+if [ -z ${hostname:+x} ]; then
+    echo "Error: cannot find host name";
+    echo "exiting...";
+    exit 1;
 fi
 
-#sudo nmap -sS -T4 -oN n_output.nmap ${subnet};
+# get subnet mask
+netmask=$(ifconfig | grep $hostname | awk '{print $4}');
+
+# parse address into parts 
+IFS=. read -r h1 h2 h3 h4 <<< $hostname;
+IFS=. read -r m1 m2 m3 m4 <<< $netmask;
+
+# bitwise AND operation on hostname and netmask to get network address
+network=$(echo "$((h1 & m1)).$((h2 & m2)).$((h3 & m3)).$((h4 & m4))");
+netprefix=$(ip addr show | grep $hostname | awk '{print $2}' | cut -d / -f 2);
+mySubnet="${network}/${netprefix}";     # CIDR notation
+
+echo;
+echo "Your local IP address is: $hostname";
+echo "Subnet mask: $netmask";
+echo "Subnet (CIDR): ${mySubnet}";
+echo;
+
+# if subnet argument not given, use host's subnet
+subnet=${1:-${mySubnet}}
+
+echo "Network scan initiated on ${subnet}:"
+#sudo nmap -sS -T4 -oN output.nmap ${subnet};
 
 # -v RS= sets the RS variable before program execution
-noOfParas=$(awk -v RS= '{print $1}' n_output.nmap | wc -l);
-for ((i=1; i<=${noOfParas}; i++))
+noOfReports=$(awk -v RS= '{print $1}' output.nmap | wc -l);
+
+table="|IP Address:\tMAC Address:\tPorts:\t\n";
+for ((i=1; i<=${noOfReports}; i++))
 do
-    if [ ! ${i} -eq ${noOfParas} ]; then
-        para=$(awk -v RS= -v lineNo=${i} 'NR==lineNo {print $0}' n_output.nmap);
+    # if not last paragraph
+    if [ ! ${i} -eq ${noOfReports} ]; then
+        # report is the scan report for one ip address
+        # gets report number i
+        report=$(awk -v RS= -v reportNo=${i} 'NR==reportNo {print $0}' output.nmap);
 
-        ipAddr=$(echo "${para}" | grep "scan report" | cut -d " " -f 5-);
-        macAddr=$(echo "${para}" | grep MAC | cut -d " " -f 3);
+        ipAddr=$(echo "${report}" | grep "scan report" | cut -d " " -f 5-);
+
+        macAddr=$(echo "${report}" | grep "MAC" | cut -d " " -f 3);
         # check if mac is empty
-        macAddr=${macAddr:-UNKNOWN}
-        openPorts=($(echo "${para}" | grep open | cut -d " " -f 1 | cut -d / -f 1));
+        macAddr=${macAddr:-UNKNOWN};
 
-        echo $ipAddr;
-        echo $macAddr;
-        for port in ${openPorts[@]}
-        do
-            echo "$port";
-        done
+        # openPorts is an array or opened ports
+        openPorts=($(echo "${report}" | grep "open" | cut -d " " -f 1 | cut -d / -f 1));
+        alt="";
+        # check if openPorts is empty
+        if [ -z ${openPorts:+x} ]; then
+            # openPorts is empty, all closed and/or filtered
+            if [[ ${report} =~ closed ]]; then
 
-        #if [ -z ${openPorts:+x} ]; then
-            # check for number of filtered and closed ports
+                if [[ ${report} =~ filtered ]]; then
+                    alt="All closed or filtered";
+                else
+                    alt="All closed";
+                fi
 
-        #fi
+            else
+                alt="All filtered";
+            fi
+        else
+            # openPorts not empty
+            openPorts=$(echo ${openPorts[@]});                # array to string
+            openPorts=$(echo ${openPorts// /, });             # replace " " with ", "
+        fi
+
+        table="${table}|${ipAddr}\t${macAddr}\t${openPorts:-${alt}}\t\n";
+    else
+        noOfIPsAndHostsUp=$(awk -v RS= 'NR==10 {print $0}' output.nmap | cut -d " " -f 11-16);
+        echo $noOfIPsAndHostsUp;
         echo;
     fi
 done
+    # $'' is a special form of quoting with backslashed escaped char 
+    # replaced as specified by the ANSI C standard.
+    charLen=$(echo -e $table | column -t -s $'\t' -o " | " | awk 'NR==1' | wc -m);
+    horizontalLine="";
+    for ((i=1; i<=((${charLen}-2)); i++)); 
+        do 
+        horizontalLine="${horizontalLine}*"; 
+    done
+
+    # print summary table
+    echo -e "$horizontalLine";
+    echo -e $table | column -t -s $'\t' -o " | ";
+    echo -e "$horizontalLine";
 
